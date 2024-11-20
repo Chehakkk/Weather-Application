9aa74b6cd44fa0a1c3b39a59d9cc44680a3f3807
Chehakkk <dilipkankariya23@gmail.com>
2024-11-20 13:27:28 +0530

    Weather Application

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "deb092f78894efe5e142466798a899e4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
